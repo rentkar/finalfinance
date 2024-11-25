25d5ea0cba0052d0b48707ba9e5157be5de1bfcc
rentkar <76766061+rentkar@users.noreply.github.com>
2024-11-26 01:38:56 +0530

    updated
    updaredd files

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,17 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   build: {
     outDir: 'dist',
-    sourcema
+    sourcemap: true
+  }
+});
